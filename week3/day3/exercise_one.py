f6e602282b92df13109e11e10b801792bee42605
alextepper <alextepper92@gmail.com>
2023-07-26 15:36:26 +0300

    oop day 3

--- week3/day3/exercise_one.py
+++ week3/day3/exercise_one.py
@@ -1,5 +1,16 @@
 # Exercise 2: Import
+import datetime
 
 from func import adding
+from func import random_string
+from func import get_current_date, days_till_date, minutes_since_birth
 
 adding(1,2)
+
+print(random_string())
+
+print(get_current_date())
+
+print(days_till_date(datetime.datetime(year=2024, month=1, day=1)))
+
+print(minutes_since_birth(datetime.datetime(1992, 6, 14)))
--- week3/day3/func.py
+++ week3/day3/func.py
@@ -1,4 +1,46 @@
+import random
+import string
+import datetime
+from datetime import date
 # Exercise 2: Import
 
 def adding(num1, num2):
     print(num1 + num2)
+
+# Exercise 3: Random Module
+def random_number(user_input):
+    num = random(1, 100)
+    if user_input in range(1, 100):
+        if num ==user_input:
+            print("Congratulations, you guessed right!")
+        else:
+            print("wrong, maybe other time")
+    else:
+        print("excuse me, your input should be in range from 1 to 100")
+
+
+# Exercise 4: String Module
+def random_string():
+    rand_string = ""
+    letters = string.ascii_letters  # Contains all uppercase and lowercase letters
+    rand_string = ''.join(random.choices(letters, k=5))
+    return rand_string
+
+# Exercise 5 : Current Date
+
+
+def get_current_date():
+    return date.today()
+
+# Exercise 6 : Amount Of Time Left Until January 1st
+
+
+def days_till_date(given_date):
+    return given_date - datetime.datetime.now()
+
+# Exercise 7 : Birthday And Minutes
+
+
+def minutes_since_birth(given_date):
+    delta = datetime.datetime.now() - given_date
+    return delta.total_seconds()/60
