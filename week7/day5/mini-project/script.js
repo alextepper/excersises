96ed2bf98bd4cbf006ea0a68ea7ae9748cd9b2ab
alextepper <alextepper92@gmail.com>
2023-08-28 17:30:22 +0300

    day5 mini-project

---
+++ week7/day5/mini-project/script.js
@@ -0,0 +1,113 @@
+const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        image: 'https://robohash.org/1?200x200'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        image: 'https://robohash.org/3?200x200'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        image: 'https://robohash.org/4?200x200'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        image: 'https://robohash.org/5?200x200'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        image: 'https://robohash.org/6?200x200'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        image: 'https://robohash.org/7?200x200'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        image: 'https://robohash.org/8?200x200'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        image: 'https://robohash.org/9?200x200'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        image: 'https://robohash.org/10?200x200'
+    }
+];
+
+const section = document.querySelector('.profiles-grid');
+
+
+robots.forEach(robot => {
+    const robotCell = document.createElement('div');
+    robotCell.classList.add('profile-card');
+    
+    const robotPic = document.createElement('img');
+    robotPic.src = robot.image
+    robotPic.classList.add('profile-pic')
+    robotName = document.createElement('h2')
+    robotName.appendChild(document.createTextNode(robot.name))
+    robotEmail = document.createElement('p')
+    robotEmail.appendChild(document.createTextNode(robot.email))
+
+    robotCell.appendChild(robotPic)
+    robotCell.appendChild(robotName)
+    robotCell.appendChild(robotEmail)
+
+    section.appendChild(robotCell);
+});
+
+function searchFunction() {
+    let filter = document.getElementById('search').value.toUpperCase();
+    
+    let cards = document.getElementsByClassName('profile-card');
+    
+    for (let i = 0; i < cards.length; i++) {
+        let name = cards[i].getElementsByTagName('h2')[0];
+        if (name) {
+            let txtValue = name.textContent || name.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                cards[i].style.display = "";
+            } else {
+                cards[i].style.display = "none";
+            }
+        }
+    }
+}
+
