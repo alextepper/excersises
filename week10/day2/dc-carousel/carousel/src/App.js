fc4866bf69ce59b28abadf24e35a6ee850722846
alextepper <alextepper92@gmail.com>
2023-09-14 11:03:02 +0300

    Carousel

---
+++ week10/day2/dc-carousel/carousel/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DemoCarousel from "./components/carousel";
+
+function App() {
+  return (
+    <div className="container-div">
+      <DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
